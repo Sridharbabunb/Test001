704c34c03673bdda38e12b05a17c3a64ba13a009
snb <sridharbabu.n.b@oracle.com>
2016-11-08 12:29:18 +0530

    First commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ * Created by snb on 11/8/2016.
+ */
+public class Test1 {
+
+    @Test
+    public void test()
+    {
+        System.out.println("Test1 funcation1");
+    }
+
+    @Test
+    public void test2()
+    {
+        System.out.println("Test1 funcation2");
+    }
+
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ * Created by snb on 11/8/2016.
+ */
+public class Test2 {
+
+    @Test
+    public void test()
+    {
+        System.out.println("Test2 funcation1");
+    }
+
+    @Test
+    public void test2()
+    {
+        System.out.println("Test2 funcation2");
+    }
+
+}
